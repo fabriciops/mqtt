5e7865c74e052ae5273480e4805b51e4e0722a65
Fabrício Pinheiro dos Santos <fabriciopps19@gmail.com>
2020-10-01 13:54:05 -0300

    ajustes

--- pub.py
+++ pub.py
@@ -1,8 +1,18 @@
 # publisher
 import paho.mqtt.client as mqtt
 
+# config_data={
+#     "temperature":"45",
+#     "humidity":"90",
+#     "location":{
+#         "latitude":"45.56565",
+#         "longitude":"88.418556"
+#     }
+# }
+
 client = mqtt.Client()
-client.connect('localhost', 1883)
+client.connect('mqtt.eclipse.org', 1883)
 
 while True:
-    client.publish("LINTANGtopic/test", input('Message : '))
+    
+    client.publish("mqtt/fabricio", input('Message : '))
